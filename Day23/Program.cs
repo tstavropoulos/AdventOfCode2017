cbb28234a9a1d10c33cacc72702c5c166ac93adf
Trevor Stavropoulos <TASagent@gmail.com>
2019-06-18 01:12:52 -0700

    Preparing puzzle 23

---
+++ Day23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day23
+{
+    class Program
+    {
+        private const string inputFile = "..\\..\\..\\..\\input23.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 23");
+            Console.WriteLine("Star 1");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Star 2");
+            Console.WriteLine("");
+
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
